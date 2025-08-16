340526d4a7db8f419db52679d5fdc226beb4ff94
Sunil Verma <sunilmacro749@gmail.com>
2025-08-16 14:43:39 +0000

    can you link this app to my firebase project. the credentials are as fol

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -4,7 +4,7 @@ export const firebaseConfig = {
   apiKey: "AIzaSyDY7-bhaci8WyY7otv-jR4rjDI753_ILHo",
   authDomain: "majime-syncflow.firebaseapp.com",
   projectId: "majime-syncflow",
-  storageBucket: "majime-syncflow.appspot.com",
+  storageBucket: "majime-syncflow.firebasestorage.app",
   messagingSenderId: "435063365257",
   appId: "1:435063365257:web:2f24e4bb4722d3fbd1f058",
   measurementId: "G-FPX64VP36R"
